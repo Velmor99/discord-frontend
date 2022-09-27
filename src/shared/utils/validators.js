d88bd0235252093e09981cb2666f85e84076dcce
Lee Roman <58044796+Velmor99@users.noreply.github.com>
2022-09-28 01:32:07 +0300

    created register form (#2)

--- src/shared/utils/validators.js
+++ src/shared/utils/validators.js
@@ -5,6 +5,10 @@ export const validateLoginForm = ({mail, password}) => {
     return isMailValid && isPasswordValid
 }
 
+export const validateRegisterForm = ({mail, password, username}) => {
+    return validateMail(mail) && validatePassword(password) && isUsernameValid(username)
+}
+
 const validatePassword = (password) => {
     return password.length > 5 && password.length <= 12;
 }
@@ -13,4 +17,8 @@ const validateMail = (mail) => {
 //  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return emailPattern.test(mail);
+}
+
+const isUsernameValid = (username) => {
+    return username.length > 2 && username.length < 13;
 }
