1c5faa700911f462d5e773f08cf30afd779018ab
Lee Roman <58044796+Velmor99@users.noreply.github.com>
2022-10-03 00:54:52 +0300

    Creating custom allert (#5)
    * created custom alert
    
    * created auth header

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 // import {composeWithDevTools} from 'redux-devtools-extension';
 import {combineReducers, applyMiddleware} from 'redux';
 import authReducer from './reducers/auth.reducer';
+import alertReducer from './reducers/alert.reducer';
 import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  alert: alertReducer
 })
 
 const store = configureStore({reducer: rootReducer})
